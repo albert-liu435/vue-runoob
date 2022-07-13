ee8bf6b7bd2d5d7f4032c791dddf72b8d0c0c111
liuxili01 <liuxili@bestseller.com.cn>
2022-07-13 15:28:20 +0800

    Vue3模板语法

---
+++ 03src-模板语法/App.vue
@@ -0,0 +1,31 @@
+<template>
+   <!-- <img alt="Vue logo" src="./assets/logo.png">-->
+   <!-- <HelloWorld></HelloWorld>-->
+    <HtmlTemplate></HtmlTemplate>
+
+</template>
+
+<script>
+    // import HelloWorld from './components/HelloWorld.vue'
+    import HtmlTemplate from "@/components/HtmlTemplate";
+
+    export default {
+        name: 'App',
+        components: {
+            // HelloWorld,
+            HtmlTemplate
+
+        }
+    }
+</script>
+
+<style>
+    #app {
+        font-family: Avenir, Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
+</style>
