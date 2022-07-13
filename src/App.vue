639e9331027e8a2709468645917647c5937ee039
liuxili01 <liuxili@bestseller.com.cn>
2022-07-13 15:27:09 +0800

    Vue3模板语法

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <template>
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld ></HelloWorld>
+   <!-- <img alt="Vue logo" src="./assets/logo.png">-->
+   <!-- <HelloWorld></HelloWorld>-->
+    <HtmlTemplate></HtmlTemplate>
 
 </template>
 
 <script>
-     import HelloWorld from './components/HelloWorld.vue'
-
+    // import HelloWorld from './components/HelloWorld.vue'
+    import HtmlTemplate from "@/components/HtmlTemplate";
 
     export default {
         name: 'App',
         components: {
-            HelloWorld
+            // HelloWorld,
+            HtmlTemplate
 
         }
     }
