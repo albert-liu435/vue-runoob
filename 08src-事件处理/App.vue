7d5c6acd291821175921ecc1734f3386d2a793cb
liuxili01 <liuxili@bestseller.com.cn>
2022-07-18 20:05:25 +0800

    Vue3 事件处理

---
+++ 08src-事件处理/App.vue
@@ -0,0 +1,40 @@
+<template>
+   <!-- <img alt="Vue logo" src="./assets/logo.png">-->
+    <div id="app">
+
+
+        <HelloWorld></HelloWorld>
+        <CommonHello></CommonHello>
+
+
+    </div>
+</template>
+
+<script>
+     import HelloWorld from './components/HelloWorld.vue'
+     import CommonHello from "@/components/CommonHello";
+     // import VforTemplate from "@/components/VforTemplate";
+    //import HtmlTemplate from "@/components/HtmlTemplate";
+
+    export default {
+        name: 'App',
+        components: {
+            CommonHello,
+            // VforTemplate,
+             HelloWorld
+            //HtmlTemplate
+
+        }
+    }
+</script>
+
+<style>
+    #app {
+        font-family: Avenir, Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
+</style>
---
+++ 08src-事件处理/components/CommonHello.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <div class="static" :class="{ 'active': isActive, 'text-danger': hasError }"></div>
+        </div>
+</template>
+
+<script>
+    export default {
+        name: "CommonHello",
+        data() {
+            return {
+                isActive: false,
+                hasError: true
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .static {
+        width: 100px;
+        height: 100px;
+    }
+    .active {
+        background: green;
+    }
+    .text-danger {
+        background: red;
+    }
+</style>
