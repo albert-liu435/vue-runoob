90166878c2bb8be2beb7a633eb2c495669496fb2
liuxili01 <liuxili@bestseller.com.cn>
2022-07-19 13:59:04 +0800

    vue3路由

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  //关闭 elsint 检测
+  lintOnSave: false
 })
